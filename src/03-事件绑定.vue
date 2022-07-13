c736fd098924ddb6dcc022305bd4d50d8669cdd2
limu <1259567994@qq.com>
2022-07-13 12:07:19 +0800

    7.13上午课程

---
+++ src/03-事件绑定.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+   <!-- 语法： v-on:事件名='少量代码'
+   语法： v-on:事件名='methods里函数名'
+   语法： v-on:事件名='methods里函数名（值）'
+   语法： @事件名='methods里函数名' -->
+
+   <p>商品总数{{count}} </p>
+   <button v-on:click="count++">+1</button>
+   <button v-on:click="btn">+1</button>
+   <button v-on:click="btn1(5)">+5</button>
+   <button @click="btn2">-1</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      count : 0
+    }
+  },
+  methods:{
+    btn() {
+      console.log(this);
+      this.count++
+    },
+    btn1(val) {
+      this.count +=val
+    },
+    btn2() {
+      this.count--
+    }
+  }
+}
+</script>
---
+++ src/07-字符串翻转练习.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- 1. 变量准备-静态页面铺设 -->
+    <h1>{{ msg }}</h1>
+    <!-- 2. 绑定点击事件 -->
+    <button @click="btn">逆转世界</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      msg: "Hello, World"
+    }
+  },
+  methods: {
+    btn() {
+      let arr = this.msg.split("");//将字符串转为数组
+      arr.reverse()//反转数组  会改变原数组
+      const str = arr.join("") //将数组拼接成字符串
+      this.msg = str
+
+      
+      // this.msg = this.msg.split("").reverse().join("");
+    }
+  }
+}
+</script>
