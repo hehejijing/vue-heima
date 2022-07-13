9f476e7e434e0062d5864fb65b04463b4a9704b4
limu <1259567994@qq.com>
2022-07-13 10:49:22 +0800

    hah

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  devServer: {
-    open: true,
-    host:"localhost",
-  },
+  // devServer: {   //服务器设置
+  //   open: true,
+  //   host:"localhost",
+  // },
   lintOnSave: false, //关闭代码检查工具 eslint
 })
